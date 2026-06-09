4695fee2700416b4530b19e23d8b85bb10fab662
Djesser <djesser.zechner@gmail.com>
2026-06-09 12:10:08 -0300

    update

--- P2_Atualizar_dados_agua_df_novo.py
+++ P2_Atualizar_dados_agua_df_novo.py
@@ -43,7 +43,7 @@ for csv in os.listdir(pasta_dados_csv):
     df.columns = dados_agua_df_vazio.columns
     dados_agua_df = pd.concat([dados_agua_df,df], ignore_index=True)
     print(csv)
-    if csv == "2026_04.csv":
+    if csv == "2026_05.csv":
         break
     else:
         pass
